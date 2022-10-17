136cdcef2cb42a5dc7ad6c12752938b9425c8c20
Anastasya <kvete@icloud.com>
2022-10-17 19:04:36 +0300

    dz8

---
+++ задача66/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу,
+ которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int Recursion(int a, int b)
+{
+    if (a==b) return a;
+    else return Recursion((a+1),b)+a;
+}
+
+int m=new Random().Next(0,20);
+int n=new Random().Next(0,20);
+if (m>n) Console.WriteLine($"сумма натуральных элементов от {n} до {m} ->{Recursion(n,m)}");
+else     Console.WriteLine($"сумма натуральных элементов от {m} до {n} ->{Recursion(m,n)}");
---
+++ задача68/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+ Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+int Recursion(int m, int n)
+{
+    if (m==0)        return n+1;
+    if (n==0)        return Recursion(m-1,1);
+    else             return Recursion(m-1,Recursion(m,n-1));
+}
+int m=0; //new Random().Next(0,3);
+Console.WriteLine(m);
+int n=2; //new Random().Next(0,8);
+Console.WriteLine(n);
+Console.WriteLine(Recursion(m,n));
